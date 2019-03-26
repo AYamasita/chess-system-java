07ba3360bf065853f0c96c11447f5820471e4442
ayamasita <alexandreyamasita@gmail.com>
2019-03-25 21:11:48 -0300

    Implementing possible moves of King.

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,30 +1,87 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
 	public King(Board board, Color color) {
 		super(board, color);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "K";
+	}
+
+	private boolean canMovie(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
 	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		// TODO Auto-generated method stub
-		
+
 		boolean mat[][] = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// above
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMovie(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// below
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMovie(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// left
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMovie(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// right
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMovie(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// northwest
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMovie(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// noreast
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMovie(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
 		
+		//southwest
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMovie(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//southeast
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMovie(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		
+		
+
 		return mat;
 	}
-	
 
 }
